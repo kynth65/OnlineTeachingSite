55a322d84599d53393f85be0309b6930eef00a03
kynth65 <kynth65@gmail.com>
2025-05-08 18:06:37 +0800

    add function for navbar when redirected it goes at the top of the page

--- react/src/components/common/Navbar.jsx
+++ react/src/components/common/Navbar.jsx
@@ -23,7 +23,9 @@ const Navbar = () => {
     const isActive = (path) => {
         return location.pathname === path;
     };
-
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location.pathname]);
     return (
         <nav
             className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
